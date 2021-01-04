6a0bd36f170eaf7c85798553e1ebe6e48f059413
Rita Cong <congrita80@gmail.com>
2021-01-04 10:21:04 -0800

    add main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# main.py
+# mightnight rider
+# a text adventure game that is riveting
+# IGN gives it 4 stars instead of 100
+
+
+# Press the green button in the gutter to run the script.
+
+def main ():
+    pass
+     #TODO : show introduction
+     # Loop
+     #TODO: check is reached the end of the game
+     #TODO:give user chooses
+
+     #TODO:change environment based on the choice and RNG
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
